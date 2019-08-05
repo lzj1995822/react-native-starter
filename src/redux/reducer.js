ce5977c32f5904a73d35dae677f744b26ae8f820
lxw <243485908@qq.com>
2019-08-05 15:34:08 +0800

    登录token

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,10 +4,12 @@ import { combineReducers } from 'redux';
 import gallery from '../modules/gallery/GalleryState';
 import app from '../modules/AppState';
 import calendar from '../modules/calendar/CalendarState';
+import token from '../modules/token/Token';
 
 export default combineReducers({
   // ## Generator Reducers
   gallery,
   app,
   calendar,
+  token
 });
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View,TextInput,Alert} from "react-native";
+import {Button, StyleSheet, Text, View,TextInput,Alert,Modal,ActivityIndicator} from "react-native";
 import { InputItem,  Icon ,List ,Toast} from '@ant-design/react-native';
 import { IconFill, IconOutline } from "@ant-design/icons-react-native";
+import blueVersion from "../styles/colors";
+import {store} from '../redux/store';
 
 const styleScope = StyleSheet.create({
     inputName: {
@@ -14,23 +16,44 @@ const styleScope = StyleSheet.create({
         height: 60,
         width:140,
         marginBottom:20,
+    },
+    overMes:{
+        height: 50,
+        backgroundColor:blueVersion.grey,
+        opacity:0.8,
+        textAlign:'center',
+        display:'flex'
+
     }
 })
 
+
 export default class Login extends React.Component {
+
 
     constructor(props: any) {
         super(props);
+        this.redux = store;
         this.state = {
             name: '',
             password:'',
+            modalVisible: false,
+            showMessage:'登陆成功',
+            token:''
         }
+
         this.submit = this.submit.bind(this);
+        this.setModalVisible = this.setModalVisible.bind(this)
     }
+
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
+    }
+
+
 
     submit() {
         let url = "http://122.97.218.162:21018/api/identity/sysUser/login"
-        console.log(this.state)
         fetch(url, {
             method: 'POST',
             headers: {
@@ -42,14 +65,31 @@ export default class Login extends React.Component {
                 password: this.state.password})
         }).then((response) => response.json()).then((resJson) => {
             if(resJson.code == 200){
+                this.redux.dispatch({
+                    type: 'SET_TOKEN',
+                    value: resJson.content.token
+                })
+                this.setState({showMessage:'登陆成功'})
+                this.setModalVisible(true)
                 this.timer = setTimeout(() => {
                     const { navigation } = this.props;
                     navigation.navigate("Main");
                     clearTimeout(this.timer);
                 }, 1000)
+            }else {
+                this.setState({showMessage:'用户名或密码错误'})
+                this.setModalVisible(true)
+                setTimeout(()=>{
+                    this.setModalVisible(false)
+                },1500)
             }
             return resJson.data;
         }).catch((error) => {
+            this.setState({showMessage:'网络错误'})
+            this.setModalVisible(true)
+            setTimeout(()=>{
+                this.setModalVisible(false)
+            },1500)
             console.error(error)
         })
     }
@@ -57,6 +97,7 @@ export default class Login extends React.Component {
     render() {
         return (
             <View>
+
                 <List renderHeader={'用户登录'}>
                     <InputItem
                         clear
@@ -91,7 +132,18 @@ export default class Login extends React.Component {
                     onPress={this.submit}
                     title="登录"
                 />
+                <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={this.state.modalVisible}
+                >
+                    <View style={styleScope.overMes}>
+                        <Text style={{textAlign: 'center',fontSize:20,marginTop:10,color:'white'}}>{this.state.showMessage}</Text>
+                    </View>
+                </Modal>
             </View>
         )
     }
 }
+
+
